6b63577070636105f0d5517bfa1fda494f43c36d
kotchamon <kotchamon.momay@gmail.com>
2018-06-26 11:52:42 +0700

    Delete messaga RUMTSV

--- src/lab1/myCode.java
+++ src/lab1/myCode.java
@@ -8,8 +8,7 @@ public class myCode {
         System.out.println("Kotchamon  ketin");
         System.out.println("361211760001");
         System.out.println("MIT211");
-        System.out.println("RMUTSV");
-        System.out.println("Hello Word");
+
 
 
 
