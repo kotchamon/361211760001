e81e60e876c7ad4c13dfbab89509e48741db03ee
kotchamon <kotchamon.momay@gmail.com>
2018-06-26 11:39:56 +0700

    Hello World

---
+++ src/lab1/myCode.java
@@ -0,0 +1,25 @@
+package lab1;
+
+public class myCode {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Word");
+        System.out.println("Kotchamon  ketin");
+        System.out.println("361211760001");
+        System.out.println("MIT211");
+        System.out.println("RMUTSV");
+
+
+
+
+
+
+    }
+
+
+
+    
+
+
+}
