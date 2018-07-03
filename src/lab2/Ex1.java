21650b8bd94a389a5abe5baad8213778458bad00
kotchamon <kotchamon.momay@gmail.com>
2018-07-03 11:15:11 +0700

    Ex1

---
+++ src/lab2/Ex1.java
@@ -0,0 +1,29 @@
+package lab2;
+
+public class Ex1 {
+    public static void main(String[] args) {
+
+        int X;
+        X = 10;
+        System.out.println(X);
+        X = 20;
+        System.out.println(X);
+        int y = 30;
+        System.out.println(y);
+        int t = X+y;
+        System.out.println(t);
+        System.out.println(y-X);
+
+
+
+
+
+
+
+
+    }//main
+
+
+
+
+}//class
