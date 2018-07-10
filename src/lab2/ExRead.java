dd3b9b295ede77b967c01ffab993b2a59468e560
kotchamon <kotchamon.momay@gmail.com>
2018-07-10 10:05:49 +0700

    input data with System.in.read()

---
+++ src/lab2/ExRead.java
@@ -0,0 +1,28 @@
+package lab2;
+
+import java.io.IOException;
+
+public class ExRead {
+    public static void main(String[] args) throws IOException {
+ //     char c;
+   //   System.out.println("Please enter one character");
+     // c = (char) System.in.read();
+      //System.out.println(c);
+
+      System.out.println("How own are you? : ");
+      int age = 0;
+       age  =(int) System.in.read();
+      System.out.println("You are "+age+"years old.");
+      }
+
+
+
+
+
+
+
+
+
+
+
+}
