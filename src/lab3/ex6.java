a1cd8e974900e717c452a0c74dd787e915ce41a2
kotchamon <kotchamon.momay@gmail.com>
2018-07-31 10:52:39 +0700

    Lab3:loop while

---
+++ src/lab3/ex6.java
@@ -0,0 +1,29 @@
+package lab3;
+
+import java.util.Scanner;
+
+public class ex6 {
+    public static void main(String[] args) {
+        //รับ input จากผู้ใช้ไปเรื่อยๆ
+        //เมื่อผู้ใช้ ใส่เลข 0จึงหยุการทำงาน
+        Scanner input = new Scanner(System.in);
+        int x=1;
+        while (x !=0){
+            System.out.println("enter an integer:   ");
+            x = input.nextInt();
+            }//while
+        System.out.println("Good bye");
+
+
+
+
+
+
+    }//main
+
+
+
+
+
+
+}//class
