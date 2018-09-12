fa97f3b23f20b0f5ba33dba50b06ed32b3ddf8c3
kotchamon <kotchamon.momay@gmail.com>
2018-09-12 23:21:15 +0700

    integer

---
+++ src/Lab5/ex3.java
@@ -0,0 +1,27 @@
+package Lab5;
+
+import java.util.Scanner;
+
+public class ex3 {
+    public static void main(String[] args) {
+         Scanner  Scanner  = new Scanner(System.in);
+        int[] num = new int[5]; //default =0
+        System.out.print("Please, input integers to array.");
+        //input
+        for (int i = 0; i < num.length; i++) {
+            System.out.print("Enter integer" + (i + 1) + ":");
+            num[i] = Scanner.nextInt();
+
+            showData(num);
+        }
+
+
+
+    }
+
+    private static void showData(int[] num) {
+        for (int val : num) {
+            System.out.print(val + "");
+        }
+    }
+}
